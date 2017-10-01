15907e70f8e22484d353b1e4a1add19c7d2e3490
Tselmeg Adiya <tselmeg9090@gmail.com>
2017-10-01 20:25:07 +0800

    Edit users and showing users are done

--- resources/views/admin/users/index.blade.php
+++ resources/views/admin/users/index.blade.php
@@ -3,7 +3,8 @@
         <table class="table">
             <thead>
               <tr>
-                <th>id</th>
+                <th>Id</th>
+                <th>Photo</th>
                 <th>Name</th>
                 <th>Email</th>
                 <th>Role</th>
@@ -17,7 +18,9 @@
                 @foreach($users as $user)
                     <tr>
                         <td>{{$user->id}}</td>
-                        <td>{{$user->name}}</td>
+                        {{--<td><img height="20px" src="/images/{{$user->photo ? $user->photo->file: 'no user photo'}}" alt=""></td>--}}
+                        <td><img height="40px" src="{{$user->photo ? $user->photo->file: 'https://placehold.it/400x400'}}" alt=""></td>
+                        <td><a href="{{route('admin.users.edit', $user->id)}}">{{$user->name}}</a></td>
                         <td>{{$user->email}}</td>
                         <td>{{$user->role ? $user->role->name : 'User has no role'}}</td>
                         <td>{{$user->is_active == 1 ? 'Active' : 'Not Active'}}</td>
